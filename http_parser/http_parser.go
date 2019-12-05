8de6ed8c971437f50f4090bb91326410dc815ca3
Neo <intelligent-fish@hotmail.com>
2019-12-05 19:03:27 +0800

    * 实现HTTP解析器

--- http_parser/http_parser.go
+++ http_parser/http_parser.go
@@ -1,1 +1,33 @@
 package http_parser
+
+// HTTP协议解析器状态
+type HTTPParserStatus int
+
+const (
+	HTTPParserStatusContinue = HTTPParserStatus(iota) // 继续
+	HTTPParserStatusError                             // 失败
+	HTTPParserStatusOK                                // 成功
+)
+
+type ValueHook func(value []byte)
+type KeyValueHook func(key, value []byte)
+
+// HTTPParser HTTP协议解析器
+type HTTPParser interface {
+	// 重置
+	Reset()
+	// 处理
+	Process(buffer []byte) HTTPParserStatus
+	// 处理方法
+	SetMethodHook(hook ValueHook) HTTPParser
+	// 处理URL
+	SetURLHook(hook ValueHook) HTTPParser
+	// 处理协议
+	SetProtocolHook(hook ValueHook) HTTPParser
+	// 处理头
+	SetHeaderHook(hook KeyValueHook) HTTPParser
+	// 处理消息体
+	SetBodyHook(hook ValueHook) HTTPParser
+	// 处理块
+	SetChunkedHook(hook ValueHook) HTTPParser
+}
--- http_parser/http_parser_test.go
+++ http_parser/http_parser_test.go
@@ -1,1 +1,23 @@
 package http_parser
+
+import "testing"
+
+func TestDefaultHttpParser(t *testing.T) {
+	httpParser := NewDefaultHTTPParser()
+	status := httpParser.SetMethodHook(func(value []byte) {
+		t.Log("method:", string(value))
+	}).SetURLHook(func(value []byte) {
+		t.Log("url:", string(value))
+	}).SetProtocolHook(func(value []byte) {
+		t.Log("protocol:", string(value))
+	}).SetHeaderHook(func(key, value []byte) {
+		t.Log("header:", string(key), string(value))
+	}).SetBodyHook(func(value []byte) {
+		t.Log("body:", string(value))
+	}).SetChunkedHook(func(value []byte) {
+	}).Process([]byte("GET / HTTP/1.1\r\nHost: localhost\r\nContent-Length: 5\r\nConnection: close\r\n\r\nhello"))
+	if HTTPParserStatusOK != status {
+		t.Error("")
+		return
+	}
+}
